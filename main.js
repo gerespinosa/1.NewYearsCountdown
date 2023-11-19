f69c3b45afc2edbec3193d51e07d0ecbc9096a2c
Ger Espinosa <espinosa.toscano@gmail.com>
2023-11-19 03:35:08 +0100

    First commit,  uploaded main files

---
+++ main.js
@@ -0,0 +1,25 @@
+const newYearString = "2024-01-01T00:00:00";
+const newYear = new Date(newYearString);
+
+console.log(newYear);
+
+function countdown() {
+  const reloj = document.getElementById("cuenta");
+  let actualDate = new Date();
+  let dif = newYear - actualDate;
+  let segundos = Math.floor(dif / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  let restoSegundos = segundos % 60;
+  let restoMinutos = minutos % 60;
+  let restoHoras = horas % 24;
+
+  let cadenaFormateada = `${dias}:${restoHoras}:${restoMinutos}:${restoSegundos}`;
+  reloj.innerHTML = cadenaFormateada;
+  console.log(cadenaFormateada);
+  return cadenaFormateada;
+}
+
+setInterval(countdown, 1000);
